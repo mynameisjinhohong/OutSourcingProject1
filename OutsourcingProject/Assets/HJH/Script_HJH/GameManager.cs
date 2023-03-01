756facceaa13a6e7609e82a80c38ff382d81f5c2
mynameisjinhohong <110793489+mynameisjinhohong@users.noreply.github.com>
2023-03-01 21:10:08 +0900

    timer제작중

--- OutsourcingProject/Assets/HJH/Script_HJH/GameManager.cs
+++ OutsourcingProject/Assets/HJH/Script_HJH/GameManager.cs
@@ -12,9 +12,17 @@ public class GameManager : MonoBehaviour
     public TMP_InputField passWordInput;
     public int passWord;
 
+    public TextMeshProUGUI timeText;
+    public List<string> scenesName;
+    public List<int> scenesList;
+    public bool loop;
+    public int time;
+
     public TextMeshProUGUI scoreText;
     public int score;
+    
     public static GameManager instance = null;
+    
     private void Awake()
     {
         DontDestroyOnLoad(gameObject);
@@ -36,7 +44,43 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         scoreText.text = "  Á¡¼ö : " + score;
+        timeText.text = time.ToString();
     }
+    public void GameStart()
+    {
+        SceneManager.LoadScene(scenesName[scenesList[0]]);
+        StartCoroutine(Timer());
+    }
+
+    IEnumerator Timer()
+    {
+        int nowTime = time;
+        int nowIdx = 1;
+        while (true)
+        {
+            yield return new WaitForSeconds(1f);
+            nowTime--;
+            if(nowTime <= 0)
+            {
+                nowTime = time;
+                nowIdx++;
+                if(nowIdx == scenesList.Count - 1)
+                {
+                    if (loop)
+                    {
+                        nowIdx = 0;
+                    }
+                    else
+                    {
+                        SceneManager.LoadScene(1);
+                        break;
+                    }
+                }
+            }
+
+        }
+    }
+
     void PassWordDone()
     {
         if(passWord == int.Parse(passWordInput.text))
